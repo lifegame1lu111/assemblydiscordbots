fc4f4ddd55bd05c4b7e4d808f580ccbfe804df27
lifegame1lu111 <40743123+lifegame1lu111@users.noreply.github.com>
2022-07-29 15:26:55 -0300

    Fixes
    Fixed bug where the handler would get stuck due to me forgetting to save callee saved register in handler procedure

--- aarch64_copycat.s
+++ aarch64_copycat.s
@@ -1,28 +1,21 @@
-.global main
+	.global main
 
-.text
+	.text
+	.align 3
 handler:
-	stp x29, x30, [sp, -16]!
-	mov x19, x0
-	mov x20, x1
-	ldr x1, [x1, 24]
-	ldr x1, [x1, 32]
-	tst x1, x1
-	b.eq not_bot
-	b bot
+	ldr x8, [x1, 24]
+	ldr x8, [x8, 32]
+	cbz x8, not_bot
+	ret
 not_bot:
 	adrp x3, msg
 	add x3, x3, :lo12:msg
-	mov x0, x19
-	ldr x1, [x20, 8]
-	ldr x2, [x20, 40]
+	ldr x2, [x1, 40]
+	ldr x1, [x1, 8]
 	str x2, [x3]
 	mov x2, x3
 	mov x3, xzr
-	bl discord_create_message
-bot:	
-	ldp x29, x30, [sp], 16
-	ret
+	b discord_create_message
 
 main:
 	stp x29, x30, [sp, -16]!
@@ -39,11 +32,12 @@ main:
 	ret
 
 .data
+	.align 3
 token:
 	.string "TOKEN GOES HERE"
-	.align 4
+	.align 3
 msg:
 	.quad 0
 	.byte 0
-	.align 3
-	.quad 0, 0, 0, 0, 0, 0, 0	
+	.zero 7
+	.quad 0, 0, 0, 0, 0, 0, 0
